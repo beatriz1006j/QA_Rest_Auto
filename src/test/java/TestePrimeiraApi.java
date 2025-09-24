4c2915ac56995670505cb5c02f2fa8c8bdc62175
Beatriz J <beatriz10j@gmail.com>
2025-09-24 11:35:51 -0300

    Exercicio 1

--- src/test/java/TestePrimeiraApi.java
+++ src/test/java/TestePrimeiraApi.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestePrimeiraApi {
 
     @Test
-    public void TesteComSucessoPrimeiraApi() {
+    public void Exe_TesteComSucessoPrimeiraApi() {
         String url = "http://localhost:8080/api/primeiraApi";
 
         RestAssured.given()
@@ -18,4 +18,24 @@ public class TestePrimeiraApi {
                     .statusCode(200)
                     .body(Matchers.equalTo("Retorno da api com sucesso!!!"));
     }
+
+    @Test
+    public void Exe_PrimeiraApiQueryParam() {
+        String url = "http://localhost:8080/api/primeiraApiV1";
+        String textoQueryParam = "Teste2";
+
+        RestAssured.given()
+
+                        //.queryParam("palavra","Teste1")
+                        .queryParam("palavra",textoQueryParam)
+                        .log().all()
+                    .when()
+                        .get(url)
+                    .then()
+                        .log().all()
+                        .assertThat() // pegar o que é esperado e retornar
+                        .statusCode(200)
+                        .body(Matchers.equalTo(textoQueryParam));
+    }
+
 }
