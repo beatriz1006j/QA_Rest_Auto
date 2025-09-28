10444373fe533c7a96d0eab85149ba512392d6dd
Beatriz J <beatriz10j@gmail.com>
2025-09-28 10:35:25 -0300

    Importar metodos staticos e reaproveitar url

--- src/test/java/TesteExercicios.java
+++ src/test/java/TesteExercicios.java
@@ -1,18 +1,25 @@
-
-
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import io.restassured.RestAssured.*; // não precisa digitar   RestAssured.given() import statico
 
+import static io.restassured.RestAssured.given;
 
 public class TesteExercicios {
 
+    String url = "http://localhost:8080/exercicios";
+
+    RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(url).build();
+
     @Test
     public void exe_PrimeiraApiParImpar() {
-        String url = "http://localhost:8080/exercicios/parOuImpar";
+        String url = "/parOuImpar";
         //String msg = "O numero 3 é impar";
 
-        RestAssured.given()
+        given()
+                        .spec(requestSpecification)
                         .queryParam("numero",2)
                         .log().all()
                     .when()
@@ -30,7 +37,8 @@ public class TesteExercicios {
 
         String url = "http://localhost:8080/exercicios/validarCpf";
 
-        RestAssured.given()
+        given()
+                        .spec(requestSpecification)
                         .queryParam("cpf","44690514038")
                         .log().all()
                     .when()
@@ -48,14 +56,15 @@ public class TesteExercicios {
 
         String url = "http://localhost:8080/exercicios/validarCpf";;
 
-        RestAssured.given()
-                .queryParam("cpf","4469051403")
-                .log().all()
-                .when()
-                .get(url)
-                .then()
-                .log().all()
-                .statusCode(500);
+        given()
+                        .spec(requestSpecification)
+                        .queryParam("cpf","4469051403")
+                        .log().all()
+                    .when()
+                        .get(url)
+                    .then()
+                        .log().all()
+                        .statusCode(500);
 
 
     }
@@ -66,16 +75,17 @@ public class TesteExercicios {
         String url = "http://localhost:8080/exercicios/parOuImpar";
         //String msg = "O numero 3 é impar";
 
-        RestAssured.given()
-                .queryParam("numero",2)
-                .log().all()
-                .when()
-                .get(url)
-                .then()
-                .log().all()
-                .statusCode(200)
-                .body(Matchers.equalToIgnoringCase("o numero 2 é par"));
-        //.body(Matchers.equalTo("O numero 2 é par"));
+       given()
+                        .spec(requestSpecification)
+                        .queryParam("numero",2)
+                        .log().all()
+                    .when()
+                        .get(url)
+                    .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body(Matchers.equalToIgnoringCase("o numero 2 é par"));
+            //.body(Matchers.equalTo("O numero 2 é par"));
     }
 
 }
--- src/test/java/TestePrimeiraApi.java
+++ src/test/java/TestePrimeiraApi.java
@@ -1,57 +1,91 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import io.restassured.RestAssured.*; // não precisa digitar   RestAssured.given() import statuico
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;  //mesmo processo retira o Matchers e usa apenas o contens
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class TestePrimeiraApi {
 
+    String url = "http://localhost:8080/api";
+
+
+    RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(url).build();
+
     @Test
     public void Exe_TesteComSucessoPrimeiraApi() {
-        String url = "http://localhost:8080/api/primeiraApi";
+        String path = "/primeiraApi";
 
-        RestAssured.given()
+        given()
+                    .spec(requestSpecification)
                     .log().all()
                 .when()
-                .   get(url)
+                    .get(path)
                 .then()
                     .log().all()
                     .assertThat() // pegar o que é esperado e retornar
                     .statusCode(200)
-                    .body(Matchers.equalTo("Retorno da api com sucesso!!!"));
+                    .body(equalTo("Retorno da api com sucesso!!!"));
     }
 
     @Test
     public void Exe_PrimeiraApiQueryParam() {
-        String url = "http://localhost:8080/api/primeiraApiV1";
-        String textoQueryParam = "Teste2";
+        String path = "/primeiraApiV1";
+        String textoQueryParam = "Teste1";
 
-        RestAssured.given()
-
+        given()
+                        .spec(requestSpecification)
+                        .queryParam("palavra", textoQueryParam)
                         .log().all()
                     .when()
-                        .get(url)
+                        .get(path)
                     .then()
                         .log().all()
                         .assertThat() // pegar o que é esperado e retornar
                         .statusCode(200)
-                        .body(Matchers.equalTo(textoQueryParam));
-    }
+                        .body(equalTo(textoQueryParam));
+        }
+
+    @Test
+    public void Exe_PrimeiraApiParam1() {
+        String textPathParam = "teste";
+        String path = "/primeiraApiV2/{variavelDigitada}" + textPathParam;
+
+
+        given()
+                        .spec(requestSpecification)
+                        .pathParam("variavelDigitada", textPathParam)
+                        .log().all()
+                    .when()
+                        .get(path)
+                    .then()
+                        .log().all()
+                        .assertThat() // pegar o que é esperado e retornar
+                        .statusCode(200)
+                        .body(containsString(textPathParam));
+        }
+
 
     @Test
     public void Exe_PrimeiraApiParam() {
-        String textPathParam = "teste3%20Academy";
-        String url = "http://localhost:8080/api/primeiraApiV2/" +textPathParam;
+        String textPathParam = "teste";
 
-
-        RestAssured.given()
+        given()
+                        .spec(requestSpecification)
+                        .pathParam("variavelDigitada", textPathParam)
                         .log().all()
                     .when()
-                        .get(url)
+                        .get("/primeiraApiV2/{variavelDigitada}")
                     .then()
                         .log().all()
-                        .assertThat() // pegar o que é esperado e retornar
+                        .assertThat()
                         .statusCode(200)
-                        .body(Matchers.equalTo(textPathParam));
+                        .body(containsString(textPathParam));
+
     }
-
-
 }
